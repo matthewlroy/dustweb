4a58f56f228572f792ff935d590f7ff246bef710
Matthew Roy <matthew@saplink.io>
2023-03-09 18:02:09 +0000

    update naming convention, add function for writing endpoints to dustchat frontend

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,51 @@
 use actix_files::Files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use std::env;
+use serde::{Deserialize, Serialize};
+use std::{env, fs::File, io::Write};
 
-enum DustAPIEndpoints {
-    HealthCheck,
+#[derive(Serialize, Deserialize)]
+struct EndpointNames {
+    health_check: &'static str,
 }
 
-impl DustAPIEndpoints {
-    fn as_str(&self) -> &'static str {
-        match self {
-            DustAPIEndpoints::HealthCheck => "/api/v1/health_check",
-        }
-    }
-}
+static API_ENDPOINTS: EndpointNames = EndpointNames {
+    health_check: "/api/v1/health_check",
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    write_api_endpoints_to_json_file()?;
+
     HttpServer::new(|| {
         App::new()
-            .route(
-                DustAPIEndpoints::HealthCheck.as_str(),
-                web::get().to(health_check),
-            )
-            .service(Files::new("/", dust_get_env_var("DUST_CHAT_PATH")).index_file("index.html"))
+            .route(API_ENDPOINTS.health_check, web::get().to(api_health_check))
+            .service(Files::new("/", get_env_var("DUST_CHAT_PATH")).index_file("index.html"))
     })
     .bind((
         "127.0.0.1",
-        dust_get_env_var("DUST_SERVER_PORT").parse::<u16>().unwrap(),
+        get_env_var("DUST_SERVER_PORT").parse::<u16>().unwrap(),
     ))?
     .run()
     .await
 }
 
-async fn health_check() -> impl Responder {
+async fn api_health_check() -> impl Responder {
     HttpResponse::Ok().body("Ok")
 }
 
-fn dust_get_env_var(desired_env_var: &str) -> String {
+fn write_api_endpoints_to_json_file() -> std::io::Result<()> {
+    let mut f = File::create(format!(
+        "{}{}",
+        get_env_var("DUST_CHAT_PATH"),
+        "endpoints_v1.json"
+    ))?;
+
+    f.write_all(serde_json::to_string(&API_ENDPOINTS).unwrap().as_bytes())?;
+
+    Ok(())
+}
+
+fn get_env_var(desired_env_var: &str) -> String {
     match env::var(desired_env_var) {
         Ok(v) => v,
         Err(e) => panic!("${} is not set ({})", desired_env_var, e),
