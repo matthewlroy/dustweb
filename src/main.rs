4f1b987e0fd16b23fb7f53487412056fa216b748
Matthew Roy <matthew@saplink.io>
2023-03-09 02:53:47 +0000

    use enum for api endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use actix_files::Files;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::env;
+
+enum DustAPIEndpoints {
+    HealthCheck,
+}
+
+impl DustAPIEndpoints {
+    fn as_str(&self) -> &'static str {
+        match self {
+            DustAPIEndpoints::HealthCheck => "/api/v1/health_check",
+        }
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .route(
+                DustAPIEndpoints::HealthCheck.as_str(),
+                web::get().to(health_check),
+            )
             .service(Files::new("/", dust_get_env_var("DUST_CHAT_PATH")).index_file("index.html"))
     })
     .bind((
@@ -14,6 +30,10 @@ async fn main() -> std::io::Result<()> {
     ))?
     .run()
     .await
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("Ok")
 }
 
 fn dust_get_env_var(desired_env_var: &str) -> String {
