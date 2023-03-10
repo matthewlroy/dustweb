43333814604d4e06ec83562b4f8938745ad11bc8
Matthew Roy <matthew@saplink.io>
2023-03-10 05:01:47 +0000

    remove endpoints from code (use from dustcfg)

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
 use actix_files::Files;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use chrono::prelude::*;
-use dustcfg::get_env_var;
+use dustcfg::{get_env_var, API_ENDPOINTS};
 use dustlog::HTTPRequestLog;
-use serde::{Deserialize, Serialize};
-use std::{fs::File, io::Write};
-
-#[derive(Serialize, Deserialize)]
-struct EndpointNames {
-    health_check: &'static str,
-}
-
-impl EndpointNames {
-    const fn new(endpoint: &'static str) -> Self {
-        EndpointNames {
-            health_check: endpoint,
-        }
-    }
-}
-
-const API_ENDPOINTS: EndpointNames = EndpointNames::new("/api/v1/health_check");
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    write_api_endpoints_to_json_file()?;
-
     HttpServer::new(|| {
         App::new()
-            .route(&API_ENDPOINTS.health_check, web::get().to(api_health_check))
+            .route(API_ENDPOINTS.health_check, web::get().to(api_health_check))
             .service(Files::new("/", get_env_var("DUST_CHAT_PATH")).index_file("index.html"))
     })
     .bind((
@@ -53,16 +34,4 @@ async fn api_health_check(req: HttpRequest) -> impl Responder {
     println!("{}", log.as_log_str());
 
     HttpResponse::Ok().body("Ok")
-}
-
-fn write_api_endpoints_to_json_file() -> std::io::Result<()> {
-    let mut f = File::create(format!(
-        "{}{}",
-        get_env_var("DUST_CHAT_PATH"),
-        "endpoints_v1.json"
-    ))?;
-
-    f.write_all(serde_json::to_string(&API_ENDPOINTS).unwrap().as_bytes())?;
-
-    Ok(())
 }
